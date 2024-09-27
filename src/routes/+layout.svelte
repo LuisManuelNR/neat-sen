e021b8155f5930b1571f31291d122a57ce78a72c
Chazy <luismanuelchazy@gmail.com>
2024-09-27 17:25:18 +0200

    basic stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import '@chasi/ui/styles/app.scss'
+	import '@chasi/ui/styles/defaultTheme.scss'
+
+	const links = [
+		{ href: '/', text: 'Home' },
+		{ href: '/builder', text: 'Network Builder' }
+	]
+</script>
+
+<nav class="s-6 mb-4 py-2 px-4">
+	{#each links as { href, text }}
+		<a class="list-item row" {href} class:selected={$page.url.pathname === href}>{text}</a>
+	{/each}
+</nav>
+
+<main class="container">
+	<slot />
+</main>
