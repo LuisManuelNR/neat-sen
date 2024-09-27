702b1338380442166167975d424641347dc5d94f
Chazy <luismanuelchazy@gmail.com>
2024-09-27 16:49:19 +0200

    migrate to svelte-kit

--- src/App.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import Entorno from './lib/Entorno.svelte'
-</script>
-
-<div class="d-grid mt-4">
-	<Entorno></Entorno>
-</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: 'modern'
-      }
-    }
-  },
-})
+	plugins: [sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
